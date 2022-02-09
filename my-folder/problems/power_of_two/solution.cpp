14e7f15a54beeabb28bcb5d91763f67fd8b4b100
Himanshu Tiwari <38527848+ozymandias9876@users.noreply.github.com>
2022-02-09 15:13:58 +0000

    Sync LeetCode submission - Power of Two (cpp)

--- my-folder/problems/power_of_two/solution.cpp
+++ my-folder/problems/power_of_two/solution.cpp
@@ -3,8 +3,11 @@ public:
     bool isPowerOfTwo(int n) {
         if(n<=0)
             return false;
-       else if(!(n&(n-1)))
-            return true;
+      long long x=floor(log2(n));
+      long long y=ceil(log2(n));
+        //cout<<x<<" "<<y;
+      if(x==y)
+          return true;
         return false;
         
         
