76d2843961911d40970e50bca6e26931cc186441
Himanshu Tiwari <38527848+ozymandias9876@users.noreply.github.com>
2022-02-20 03:08:41 +0000

    Sync LeetCode submission - Merge Nodes in Between Zeros (cpp)

---
+++ my-folder/problems/merge_nodes_in_between_zeros/solution.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeNodes(ListNode* head) {
+        ListNode* res=head;
+     
+
+    ListNode *temp = head;
+    int sum = 0;
+        temp=temp->next;
+        res=res->next;
+     
+    while(temp)
+    {
+         
+   
+        if(temp->val != 0)
+        {
+            sum += temp->val;
+            temp = temp->next;
+        }
+         
+   
+        else
+        {
+            res->val = sum;
+            res->next = temp->next;
+            temp = temp->next;
+            res = temp;
+            sum = 0;
+        }
+    }
+        return head->next;
+        
+    }
+};
