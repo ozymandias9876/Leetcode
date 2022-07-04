4b11b2fcba1d0ad5c37ff205ec710d69e28f8059
Himanshu Tiwari <38527848+ozymandias9876@users.noreply.github.com>
2022-07-04 12:55:48 +0000

    Sync LeetCode submission - Permutations (cpp)

--- my-folder/problems/permutations/solution.cpp
+++ my-folder/problems/permutations/solution.cpp
@@ -1,13 +1,31 @@
 class Solution {
 public:
+    static void perm(int arr[],vector<int>nums,vector<int>&ds,vector<vector<int>>&ans)
+    {
+        if(ds.size()==nums.size())
+        {
+            ans.push_back(ds);
+            return;
+        }
+        for(int i=0;i<nums.size();i++)
+        {
+            if(arr[i]==0)
+            {
+                ds.push_back(nums[i]);
+                arr[i]=1;
+                perm(arr,nums,ds,ans);
+                ds.pop_back();
+                arr[i]=0;
+                
+            }
+        }
+    }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>>pm;
-        
-        sort(nums.begin(),nums.end());
-        pm.push_back(nums);
-        while(next_permutation(nums.begin(),nums.end()))
-            pm.push_back(nums);
-        //sort(pm.begin(),pm.end());
+        int arr[nums.size()];
+        fill_n(arr,nums.size(),0);
+        vector<int>ds;
+        perm(arr,nums,ds,pm);
         return pm;
     }
 };
