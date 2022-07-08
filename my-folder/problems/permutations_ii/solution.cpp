af3628af5020a73d65aeaf3af21d1a574733b2b2
Himanshu Tiwari <38527848+ozymandias9876@users.noreply.github.com>
2022-07-08 17:22:19 +0000

    Sync LeetCode submission - Permutations II (cpp)

--- my-folder/problems/permutations_ii/solution.cpp
+++ my-folder/problems/permutations_ii/solution.cpp
@@ -1,19 +1,26 @@
 class Solution {
 public:
+
+static void perm(int ind,vector<int>nums,vector<vector<int>>&ans)
+{
+    if(ind==nums.size())
+    {
+        ans.push_back(nums);
+    }
+    for(int i=ind;i<nums.size();i++)
+    {
+        if(i>ind and nums[i]==nums[ind])
+            continue;
+        swap(nums[i],nums[ind]);
+        perm(ind+1,nums,ans);
+        //swap(nums[i],nums[ind]);
+    }
+}
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         vector<vector<int>> x;
         sort(nums.begin(),nums.end());
-
-        x.push_back(nums);
-        //sv.insert(nums);
-        set<vector<int>>s;
-        while(next_permutation(nums.begin(),nums.end()))
-            s.insert(nums);
-        for(auto i:s)
-        {
-            x.push_back(i);
-                
-        }
+        
+        perm(0,nums,x);
         return x;
     }
 };
